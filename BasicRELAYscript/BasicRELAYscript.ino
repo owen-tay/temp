8c4318ba95316c4b03fc04b95dcec8284c850e93
owentay <owentaylordev@gmail.com>
2025-05-13 23:46:32 +0100

    testt!

--- BasicRELAYscript/BasicRELAYscript.ino
+++ BasicRELAYscript/BasicRELAYscript.ino
@@ -1,5 +1,7 @@
 int LIGHTRELAY = 4;
+int PUMPRELAY = 5;
 
+//test for now. Will creat 24 clock using hours mins etc
 void setup() {
   pinMode(LIGHTRELAY, OUTPUT);
   digitalWrite(LIGHTRELAY, HIGH);  
@@ -9,5 +11,9 @@ void loop() {
   digitalWrite(LIGHTRELAY, LOW);   
   delay(500);
   digitalWrite(LIGHTRELAY, HIGH); 
+  delay(500);
+  digitalWrite(PUMPRELAY, LOW);   
+  delay(500);
+  digitalWrite(PUMPRELAY, HIGH); 
   delay(500);
 }
